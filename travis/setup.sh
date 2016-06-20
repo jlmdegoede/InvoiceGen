0435b968ca9d30c2bf70b7c080a76aee05115adf
Jochem <jochem@degoede.email>
2016-06-20 15:45:43 +0200

    Travis CI fix

--- travis/setup.sh
+++ travis/setup.sh
@@ -1,4 +1,5 @@
 echo 'ALLOWED_HOSTS = ["test.invoicegen.nl"]
 SECRET_KEY = "8w9eruajfhsdjfl3h4uihdfwekrw4t2rgergsergsetg"
-DEBUG = True' > InvoiceGen/site_settings.py
+DEBUG = True
+COMMUNICATION_KEY = "2985uo2438porjirefjsf2435o34t"'> InvoiceGen/site_settings.py
 
