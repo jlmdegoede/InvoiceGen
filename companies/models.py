ecc8ecaab754b0a9cac5c83b978706defddaf389
Jochem <jochem@degoede.email>
2017-07-17 19:37:21 +0200

    Made fields email and default price for Company optional

--- companies/models.py
+++ companies/models.py
@@ -5,8 +5,8 @@ class Company(models.Model):
     company_name = models.CharField(max_length=200)
     company_address = models.CharField(max_length=200)
     company_city_and_zipcode = models.CharField(max_length=200)
-    company_email = models.CharField(max_length=200, null=True)
-    company_default_price_per_quantity = models.FloatField(null=True)
+    company_email = models.CharField(max_length=200, null=True, blank=True)
+    company_default_price_per_quantity = models.FloatField(null=True, blank=True)
 
     def __str__(self):
         return self.company_name
