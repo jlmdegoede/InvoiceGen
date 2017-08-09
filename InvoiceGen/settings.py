0d3c81e984eaf15a115ef1bef1003853b9b12ad4
Jochem <jochem@degoede.email>
2017-08-09 15:27:13 +0200

    django-autoadmin toegevoegd
    Voor betere gebruiksvriendelijkheid
    Docker bijgewerkt accordingly

--- InvoiceGen/settings.py
+++ InvoiceGen/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.humanize',
     'channels',
     'rest_framework',
+    'autoadmin'
 )
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 python3 manage.py migrate
 python3 manage.py loaddata fixtures/*.json
+python3 manage.py createautoadmin
 daphne -b 0.0.0.0 -p 8000 InvoiceGen.asgi:channel_layer
