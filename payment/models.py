4840cbd10aa15bd635fcc9f0f0a46b390db78758
Jochem <jochem@degoede.email>
2017-08-13 11:38:55 +0200

    Payment-model aangemaakt

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+from invoices.models import OutgoingInvoice
+from companies.models import Company
+
+
+class Payment(models.Model):
+    PAID = 'paid'
+    CANCELLED = 'cancelled'
+    PENDING = 'pending'
+    PAYMENT_STATUS = (
+        (PAID, 'Betaald'),
+        (CANCELLED, 'Geannuleerd'),
+        (PENDING, 'Gemaakt')
+    )
+
+    created = models.DateTimeField(auto_now_add=True)
+    for_invoice = models.ForeignKey(to=OutgoingInvoice)
+    status = models.CharField(max_length=20, choices=PAYMENT_STATUS)
+    payment_amount = models.DecimalField()
+
+
+class BunqRequest(models.Model):
+    counterparty_email = models.EmailField(null=True)
+    counterparty_iban = models.CharField(max_length=34, null=True)
+    counterparty_phone = models.CharField(max_length=34, null=True)
+    to_company = models.ForeignKey(to=Company)
+    bunq_request_id = models.IntegerField()
+    description = models.TextField(blank=True)
